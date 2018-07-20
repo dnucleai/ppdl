1e7ad873541742496803b12e05ae734d5384ed38
Benjamin Kha <ben.kha@outlook.com>
2018-07-20 06:45:45 -0400

    Update deltas after every epoch

--- ppdl/storage/local_gradient_storage.py
+++ ppdl/storage/local_gradient_storage.py
@@ -11,22 +11,24 @@ class LocalGradientStorage(GradientStorage):
         self.filename = filename
 
     # TODO: This should be moved to server side so clients can't reinitialize gradients
-    def initialize_gradients(self, shape):
+    def initialize_weights(self, shape):
         gradients = np.zeros(shape)
         with open(self.filename, 'wb') as f:
             pickle.dump(gradients)
 
-    def store_gradients(self, indices, gradients):
+    def update_weights(self, deltas):
         with open(self.filename, 'wb') as f:
-            all_gradients = self.fetch_all_gradients()
-            all_gradients[indices] = gradients
-            pickle.dump(all_gradients)
+            all_weights = self.fetch_all_weights()
+            new_weights = []
+            for weight, delta in zip(all_weights, deltas):
+                new_weights.append(weight + delta)
+            pickle.dump(new_weights)
 
-    def fetch_all_gradients(self):
+    def fetch_all_weights(self):
         with open(self.filename, 'r') as f:
             return pickle.load(f)
 
-    def fetch_gradients(self, indices):
+    def fetch_weights(self, indices):
         with open(self.filename, 'r') as f:
             all_gradients = pickle.load(f)
             return all_gradients[indices]
