ac4f1f9ed514a71430214fffe1e121ae0f6458f1
Benjamin Kha <ben@nucle.ai>
2018-08-11 19:46:06 -1000

    HTTP Parameter Manager for web server implementation

--- ppdl/client/parameter_manager.py
+++ ppdl/client/parameter_manager.py
@@ -4,9 +4,9 @@ from abc import ABCMeta, abstractmethod
 class ParameterManager(metaclass=ABCMeta):
 
     @abstractmethod
-    def upload_deltas(self, deltas, client_id):
+    def upload_deltas(self, client_id, deltas):
         pass
 
     @abstractmethod
-    def download_params(self):
+    def download_params(self, client_id):
         pass
